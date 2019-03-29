a6ea545afcd1886fe04beb6e13a9cbcc409024a9
bb <baibin@digifinex.com>
2019-03-29 18:48:07 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+const PostImport  = require('postcss-import')
+const PostUrl = require('postcss-url')
 
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? './'
-        : '/'
+        : '/',
+    configureWebpack: {
+        plugins: [
+            new PostImport(),
+            new PostUrl()
+        ]
+    }
 }
